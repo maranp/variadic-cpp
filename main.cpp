4c866fb458f3921e291c5004be8f7e87869c930b
Maran Pakkirisamy <mpakkirisamy@caveonetworks.com>
2017-09-21 09:37:58 +0530

    handling variadic templates stage 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+
+template <size_t n, typename T1, typename... Ts>
+auto & get_value(T1 && t1, Ts &&... ts) {
+  return t1;
+}
+
 int main() {
+  std::cout << "hello" << std::endl;
+  std::cout << get_value<2>(9, 5, 6) << std::endl;
   return 0;
 }
